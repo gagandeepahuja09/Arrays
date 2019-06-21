0e00799ad04e86ae3bd21c77135419a3e90b82a1
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-21 11:27:36 +0530

    very good question

--- InterviewBit/NextPermutation.cpp
+++ InterviewBit/NextPermutation.cpp
@@ -1,22 +1,46 @@
-void Solution::nextPermutation(vector<int> &A) {
-        // Do not write main() function.
-        // Do not read input, instead use the arguments to the function.
-        // Do not print the output, instead return values as specified
-        // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-        int n = A.size(), i;
-        for(i = n-2; i>=0; i--)
-            if(A[i] < A[i+1])
-                break;
-        if(i < 0) {
-            reverse(A.begin(), A.end());
-            return;
-        }
-        int smallest = i+1;
-        for(int j = i+2; j<n; j++)
-            if(A[j] > A[i] && A[j] < A[smallest])
-                smallest = j;
-        swap(A[smallest], A[i]);
-        sort(A.begin() + i + 1, A.end());
-    }
+/*
+Next Permutation
 
+Implement the next permutation, which rearranges numbers into the numerically next greater permutation of numbers.
+
+If such arrangement is not possible, it must be rearranged as the lowest possible order ie, sorted in an ascending order.
+
+The replacement must be in-place, do not allocate extra memory.
+
+Examples:
+
+1,2,3 → 1,3,2
+
+3,2,1 → 1,2,3
+
+1,1,5 → 1,5,1
+
+20, 50, 113 → 20, 113, 50
+*/
+
+// This is the region which cannot be increased to next permutation
+int idxPeak(vector<int>& A) {
+    int i = A.size() - 1;
+    while(A[i] <= A[i - 1]) {
+        i--;
+    }
+    return i;
+}
+
+int lastIdxOfGrt(vector<int>& A, int val) {
+    int i = A.size() - 1;
+    while(A[i] <= val) {
+        i--;
+    }
+    return i;
+}
+
+void Solution::nextPermutation(vector<int> &A) {
+    int pivot = idxPeak(A) - 1;
+    if(pivot != -1) {
+        int successor = lastIdxOfGrt(A, A[pivot]);
+        swap(A[pivot], A[successor]);
+    }
+    reverse(A.begin() + pivot + 1, A.end());
+}
 
