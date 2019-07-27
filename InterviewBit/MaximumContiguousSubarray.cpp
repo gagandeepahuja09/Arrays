b4e49e8ce945789227270318e07c92f8b781d92c
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-27 10:31:24 +0530

    easy

--- InterviewBit/MaximumContiguousSubarray.cpp
+++ InterviewBit/MaximumContiguousSubarray.cpp
@@ -1,16 +1,9 @@
 int Solution::maxSubArray(const vector<int> &A) {
-    int maxSum = INT_MIN, currMax = 0;
-    for(int i = 0; i < A.size(); i++) {
-        currMax += A[i];
-        if(currMax < 0)
-            currMax = 0;
-        if(currMax > 0)    
-            maxSum = max(maxSum, currMax);
+    int currMax = A[0], retMax = A[0];
+    for(int i = 1; i < A.size(); i++) {
+        currMax = max(A[i], currMax + A[i]);
+        retMax = max(retMax, currMax);
     }
-    if(maxSum == INT_MIN) {
-        for(int i = 0; i < A.size(); i++)
-            maxSum = max(maxSum, A[i]);
-    }
-    return maxSum;
+    return retMax;
 }
 
