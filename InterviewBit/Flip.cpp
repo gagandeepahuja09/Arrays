52d5418f7b883c3112a2a76bf4280eba3fad2b8d
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-27 18:49:14 +0530

    imp question

--- InterviewBit/Flip.cpp
+++ InterviewBit/Flip.cpp
@@ -1,28 +1,21 @@
-// Similar To Kadane
-
 vector<int> Solution::flip(string A) {
-    int n = A.size();
-    vector<int> res(2, -1);
-    int start = 0, end = 0, currSum = 0, maxSum = 0;
-    for(int i=0; i<n; i++) {
-        if(A[i] == '0')
-            currSum++;
-        else
-            currSum--;
-        if(currSum < 0) {
-            currSum = 0;
+    int mx = 0, cnt = 0, start = 0;
+    vector<int> ret(2, -1);
+    for(int i = 0; i < A.size(); i++) {
+        cnt += (A[i] == '0' ? 1 : -1);
+        if(cnt < 0) {
+            cnt = 0;
             start = i + 1;
         }
-        if(currSum > maxSum) {
-            maxSum = currSum;
-            res[0] = start;
-            res[1] = end = i;
+        if(cnt > mx) {
+            mx = cnt;
+            ret[0] = start;
+            ret[1] = i;
         }
     }
-    if(res[0] == -1 && res[1] == -1)
+    if(ret[0] == -1)
         return {};
-    
-    res[0]++; res[1]++;    
-    return res;    
+    ret[0]++; ret[1]++;    
+    return ret;    
 }
 
