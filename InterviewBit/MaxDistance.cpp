6c95e30e6eacbb8b2846b21daba8b95de35c7c28
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-29 11:16:17 +0530

    easy

--- InterviewBit/MaxDistance.cpp
+++ InterviewBit/MaxDistance.cpp
@@ -1,24 +1,25 @@
 int Solution::maximumGap(const vector<int> &A) {
     int n = A.size();
-    if(n <= 1)
-        return 0;
-    vector<int> lMin(n), rMax(n);
+    vector<int> lMin(n, 0);
+    vector<int> rMax(n, 0);
     lMin[0] = A[0];
-    for(int i=1; i<n; i++) {
-        lMin[i] = min(lMin[i-1], A[i]);
+    for(int i = 1; i < n; i++) {
+        lMin[i] = min(A[i], lMin[i - 1]);
     }
-    rMax[n-1] = A[n-1];
-    for(int i=n-2; i>=0; i--) {
-        rMax[i] = max(rMax[i+1], A[i]);
+    rMax[n - 1] = A[n - 1];
+    for(int i = n - 2; i >= 0; i--) {
+        rMax[i] = max(A[i], rMax[i + 1]);
     }
-    int i = 0, j = 0, maxDiff = -1;
+    int maxDiff = 0;
+    int i = 0, j = 0;
     while(i < n && j < n) {
         if(lMin[i] <= rMax[j]) {
-            maxDiff = max(maxDiff, j-i);
+            maxDiff = max(maxDiff, j - i);
             j++;
         }
-        else
+        else {
             i++;
+        }
     }
     return maxDiff;
 }
