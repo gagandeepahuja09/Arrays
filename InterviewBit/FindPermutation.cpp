fdfb3710a93467f8ce9d0fb69046641c20379c18
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-30 18:30:42 +0530

    very good question

--- InterviewBit/FindPermutation.cpp
+++ InterviewBit/FindPermutation.cpp
@@ -1,15 +1,23 @@
-vector<int> Solution::findPerm(const string A, int B) {
-    vector<int> res(A.size() + 1);
-    int count = 1;
-    for(int i=0; i<=A.size(); i++) {
-        if(A[i] == 'I' || i == A.size()) {
-            for(int j = i-1; j>=-1; j--) {
-                res[j+1] = count++;
-                if(j >= 0 && A[j] == 'I')
-                    break;
-            }
-        } 
+void rev(vector<int>& ret, int i, int j) {
+    while(i < j) {
+        swap(ret[i], ret[j]);
+        i++;    j--;
     }
-    return res;
+}
+
+vector<int> Solution::findPerm(const string A, int B) {
+    int n = A.size();
+    vector<int> ret(n + 1);
+    for(int i = 0; i < ret.size(); i++)
+        ret[i] = i + 1;
+    int i = 0;     
+    while(i <= n) {
+        int j = i;
+        while(i < n && A[i] == 'D')
+            i++;
+        rev(ret, j, i);
+        i++;
+    }
+    return ret;
 }
 
