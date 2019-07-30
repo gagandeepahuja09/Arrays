996556bd0749bcca7369df006b63e2350e6fb6a5
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-30 19:43:48 +0530

    good & v. imp

--- InterviewBit/FirstMissingPositive.cpp
+++ InterviewBit/FirstMissingPositive.cpp
@@ -1,17 +1,14 @@
 int Solution::firstMissingPositive(vector<int> &A) {
-    for(int i=0; i < A.size(); i++) {
-        if(A[i] > A.size() || A[i] <= 0 || A[i] == i+1)
+    for(int i = 0; i < A.size(); i++) {
+        if(A[i] == i + 1 || A[i] > A.size() || A[i] <= 0 
+        || A[i] == A[A[i] - 1])
             continue;
-        else {
-            if(A[A[i] - 1] == A[i])
-                continue;
-            swap(A[i], A[A[i] - 1]);
-            i--;
-        }    
+        swap(A[i], A[A[i] - 1]);
+        i--;
     }
-    for(int i=0; i<A.size(); i++) {
-        if(A[i] != i+1)
-            return i+1;
+    for(int i = 0; i < A.size(); i++) {
+        if(A[i] != i + 1)
+            return i + 1;
     }
     return A.size() + 1;
 }
