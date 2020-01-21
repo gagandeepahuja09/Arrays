1272542196bb1dcfcee225432c3445adea270a2e
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-01-21 12:40:54 +0530

    EASY revise

--- LeetCode/Maximum Sum Circular Subarray.cpp
+++ LeetCode/Maximum Sum Circular Subarray.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& A) {
-        int maxSum = -30001, minSum = 30001, currMax = 0, currMin = 0, total = 0;
-        for(int a : A) {
-            currMax = max(a, currMax + a);
-            currMin = min(a, currMin + a);
+        int maxSum = A[0], minSum = A[0], currMax = A[0], currMin = A[0], total = A[0];
+        for(int i = 1; i < A.size(); i++) {
+            currMax = max(A[i], currMax + A[i]);
+            currMin = min(A[i], currMin + A[i]);
             maxSum = max(maxSum, currMax);
             minSum = min(minSum, currMin);
-            total += a;
+            total += A[i];
         }
         return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
     }
