4d3337e21482aa56ff0d3958d887dc82d3437a01
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-03-21 22:11:40 +0530

    very easy

---
+++ LeetCode/Find The Distance Value Between Two Arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int count = 0;
+        for(int i = 0; i < arr1.size(); i++) {
+            int flag = 1;
+            for(int j = 0; j < arr2.size(); j++) {
+                if(abs(arr1[i] - arr2[j]) <= d) {
+                    flag = 0;
+                    break;
+                }
+            }
+            count += flag;
+        }
+        return count;
+    }
+};
